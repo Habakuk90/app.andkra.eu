9ae50cf289dba3aded9d5d1a0b44e0eb0df97acc
Andreas Krahl <andreas.krahl@netzkern.de>
2019-11-03 07:17:09 +0100

    changed to ap.andkra.eu docker hub;

--- scripts/docker-deploy.sh
+++ scripts/docker-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build --build-arg key=${GHOST_API_KEY} -t wahnsinnshub/ttt-repo:latest .
-docker push wahnsinnshub/ttt-repo:latest
+docker build --build-arg key=${GHOST_API_KEY} -t wahnsinnshub/app.andkra.eu:latest .
+docker push wahnsinnshub/app.andkra.eu:latest
 
 # TODO If we are build as release we want to push by the latest release number
